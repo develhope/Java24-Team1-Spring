77a7ce9926426332aaeb8af73dd13d94b1303482
Giada196 <jadeinwonderland22@gmail.com>
2024-06-07 11:31:32 +0200

    Controller per user iscritti

--- src/main/java/com/develhope/spring/DAO/IscrizioneDAO.java
+++ src/main/java/com/develhope/spring/DAO/IscrizioneDAO.java
@@ -2,8 +2,16 @@ package com.develhope.spring.DAO;
 
 import com.develhope.spring.entities.Iscrizione;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IscrizioneDAO extends JpaRepository<Iscrizione, Long> {
+
+    @Query("SELECT i FROM Iscrizione i WHERE i.user.id = :id")
+    List<Iscrizione> findCourseByUser(@Param("id") Long id);
+
 }
--- src/main/java/com/develhope/spring/services/CourseService.java
+++ src/main/java/com/develhope/spring/services/CourseService.java
@@ -113,5 +113,4 @@ public class CourseService {
         return courseDTOList;
 
     }
-
 }
